0cff1a21ecb6f5dc485e6d7091e500777eea0d16
Sergey Grishin <sergey.grishin.dev@gmail.com>
2018-10-24 17:34:51 +0300

    minor changes after changing package names and other similar changes

--- src/main/kotlin/com/sagrishin/smartreader/core/data/database/dao/impl/GenresDaoImpl.kt
+++ src/main/kotlin/com/sagrishin/smartreader/core/data/database/dao/impl/GenresDaoImpl.kt
@@ -1,19 +1,47 @@
 package com.sagrishin.smartreader.core.data.database.dao.impl
 
 import com.sagrishin.smartreader.core.data.database.dao.GenresDao
+import com.sagrishin.smartreader.core.data.database.entities.DatabaseGenre
 import com.sagrishin.smartreader.core.data.database.entities.GenreEntity
 import com.sagrishin.smartreader.core.data.database.entities.Genres
-import com.sagrishin.smartreader.core.data.repositories.models.Genre
+import com.sagrishin.smartreader.core.data.database.exceptions.DuplicatedDataInDatabaseException
+import com.sagrishin.smartreader.core.data.database.exceptions.NothingFoundInDatabaseException
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.stream.Collectors
 
-class GenresDaoImpl(db: Database) : GenresDao {
+class GenresDaoImpl : GenresDao {
 
-    private val db = db
+    private val db: Database
 
-    override fun getAllGenres(): List<GenreEntity> {
-        return transaction (db) { Genres.selectAll().map { GenreEntity(genre = it[Genres.genre]) } }
+    constructor(db: Database) {
+        this.db = db
+    }
+
+    override fun getAllGenres(): List<DatabaseGenre> {
+        return transaction (db) { GenreEntity.all().toList()
+                .parallelStream()
+                .map { DatabaseGenre(it.id.value, it.genre, it.link) }
+                .collect(Collectors.toList())
+        }
+    }
+
+    @Throws(NothingFoundInDatabaseException::class)
+    override fun findGenreByName(genreName: String): DatabaseGenre {
+        return transaction(db) { getGenreByName(genreName) }
+    }
+
+    @Throws(NothingFoundInDatabaseException::class)
+    private fun getGenreByName(genreName: String): DatabaseGenre {
+        return try {
+            val foundGenre = GenreEntity.find { Genres.genre eq genreName }.single()
+            DatabaseGenre(foundGenre.id.value, foundGenre.genre, foundGenre.link)
+        } catch (e: Exception) {
+            throw NothingFoundInDatabaseException("no one genre found by '$genreName'")
+        } catch (e: NoSuchElementException) {
+            val m = "Duplicates of genre with name '$genreName'"
+            throw DuplicatedDataInDatabaseException(m)
+        }
     }
 
 }
--- src/main/kotlin/com/sagrishin/smartreader/di/modules/ControllersModule.kt
+++ src/main/kotlin/com/sagrishin/smartreader/di/modules/ControllersModule.kt
@@ -1,15 +1,22 @@
 package com.sagrishin.smartreader.di.modules
 
 import com.google.gson.Gson
+import com.sagrishin.smartreader.controllers.BooksController
 import com.sagrishin.smartreader.controllers.GenresController
+import com.sagrishin.smartreader.controllers.impl.BooksControllerImpl
 import com.sagrishin.smartreader.controllers.impl.GenresControllerImpl
 import com.sagrishin.smartreader.core.data.database.SmartReaderDatabase
+import com.sagrishin.smartreader.core.data.database.dao.GenresDao
+import com.sagrishin.smartreader.core.data.database.dao.impl.BooksDaoImpl
 import com.sagrishin.smartreader.core.data.database.dao.impl.GenresDaoImpl
+import com.sagrishin.smartreader.core.data.repositories.BooksRepositoryImpl
+import com.sagrishin.smartreader.core.data.repositories.GenresRepositoryImpl
 import com.sagrishin.smartreader.core.threads.Executor
-import com.sagrishin.smartreader.models.GenresModel
-import com.sagrishin.smartreader.models.impl.GenresRepositoryImpl
+import com.sagrishin.smartreader.models.impl.BooksModelImpl
+import com.sagrishin.smartreader.models.impl.GenresModelImpl
 import dagger.Module
 import dagger.Provides
+import org.jetbrains.exposed.sql.Database
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -17,15 +24,29 @@ import javax.inject.Singleton
 @Singleton
 class ControllersModule {
 
-    private val db = SmartReaderDatabase.getDatabaseInstance()
+    private val db: Database
+    private val genresDao: GenresDao
+
+    init {
+        db = SmartReaderDatabase.getDatabaseInstance()
+        genresDao = GenresDaoImpl(db)
+    }
 
     @Provides
     @Singleton
     fun getGenresController(gson: Gson, @Named("OneThreadExecutor") threadExecutor: Executor): GenresController {
-        val dao = GenresDaoImpl(db)
-        val repo = GenresRepositoryImpl(dao)
-        val model = GenresModel(repo, threadExecutor)
+        val repo = GenresRepositoryImpl(genresDao)
+        val model = GenresModelImpl(repo, threadExecutor)
         return GenresControllerImpl(model, gson)
+    }
+
+    @Provides
+    @Singleton
+    fun getBooksController(gson: Gson, @Named("OneThreadExecutor") threadExecutor: Executor): BooksController {
+        val dao = BooksDaoImpl(db, genresDao)
+        val repo = BooksRepositoryImpl(dao)
+        val model = BooksModelImpl(repo, threadExecutor)
+        return BooksControllerImpl(model, gson)
     }
 
 }
