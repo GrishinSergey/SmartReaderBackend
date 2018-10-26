9ef889fa12d79c423747507f77a82c7b812f00b2
Sergey Grishin <sergey.grishin.dev@gmail.com>
2018-10-26 23:43:17 +0300

    [ADD] added genres repository test

---
+++ src/test/kotlin/com/sagrishin/smartreader/models/repositories/GenresRepositoryImplTest.kt
@@ -0,0 +1,48 @@
+package com.sagrishin.smartreader.models.repositories
+
+import com.sagrishin.smartreader.core.data.models.DatabaseGenre
+import com.sagrishin.smartreader.core.data.database.dao.impl.GenresDaoImpl
+import com.sagrishin.smartreader.core.data.repositories.GenresRepositoryImpl
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert
+import org.junit.Test
+
+class GenresRepositoryImplTest {
+
+    @Test
+    fun getAllGenres() {
+        val genres = listOf(
+                DatabaseGenre(genre = "Genre1"),
+                DatabaseGenre(genre = "Genre2"),
+                DatabaseGenre(genre = "Genre3"),
+                DatabaseGenre(genre = "Genre4"),
+                DatabaseGenre(genre = "Genre5"),
+                DatabaseGenre(genre = "Genre6")
+        )
+
+        val mockedDao = mockk<GenresDaoImpl>()
+        every { mockedDao.getAllGenres() } returns genres andThenThrows Exception("")
+
+        val genresRepository = GenresRepositoryImpl(mockedDao)
+
+        val allGenres = genresRepository.getAllGenres()
+
+        (0 until genres.size).forEach {
+            val preparedGenre = genres[it]
+            val selectedGenre = allGenres[it]
+
+            Assert.assertEquals(preparedGenre.genre, selectedGenre.genre)
+        }
+
+        val cachedGenres = genresRepository.getAllGenres()
+
+        (0 until genres.size).forEach {
+            val preparedGenre = genres[it]
+            val selectedGenre = cachedGenres[it]
+
+            Assert.assertEquals(preparedGenre.genre, selectedGenre.genre)
+        }
+    }
+
+}
