553ba3e597cd00cc0c3aa78c1d3ead9ce37cbd16
Sergey Grishin <sergey.grishin.dev@gmail.com>
2018-10-29 20:21:52 +0200

    [UPD] added new http codes in Controller.kt

--- src/main/kotlin/com/sagrishin/smartreader/controllers/Controller.kt
+++ src/main/kotlin/com/sagrishin/smartreader/controllers/Controller.kt
@@ -4,6 +4,9 @@ interface Controller {
 
     companion object {
         const val SUCCESS_STATUS_CODE = 200
+        const val BAD_REQUEST_CODE = 400
+        const val NOT_FOUND_CODE = 404
+        const val SERVER_ERROR_CODE = 500
     }
 
 }
