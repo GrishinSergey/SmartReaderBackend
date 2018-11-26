56bad2e06f49d3fb541b643aeeb149b0bee26042
Sergey Grishin <sergey.grishin.dev@gmail.com>
2018-11-26 21:41:29 +0200

    [FIX] add encoding to database connection url, so now it correctly work with cyrillic and latin names of books, users etc

--- src/main/kotlin/com/sagrishin/smartreader/core/data/database/SmartReaderDatabase.kt
+++ src/main/kotlin/com/sagrishin/smartreader/core/data/database/SmartReaderDatabase.kt
@@ -9,24 +9,39 @@ object SmartReaderDatabase {
     const val DATABASE_NAME = "smartyreader"
     const val DATABASE_USER = "root"
     const val DATABASE_PASSWORD = "1111"
+
     const val DATABASE_DRIVER = "com.mysql.jdbc.Driver"
     const val DATABASE_TYPE = "mysql"
+
     const val DATABASE_HOST = "localhost"
     const val DATABASE_PORT = "3306"
 
+    const val CONNECTION_ENCODING = "characterEncoding=utf8"
+    const val USE_UNICODE = "useUnicode=true"
+
     fun getDatabaseInstance(): Database {
         val connect = Database.connect(
-                url = "jdbc:$DATABASE_TYPE://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME",
+                url = getDatabaseServerUrlConnection(),
                 driver = DATABASE_DRIVER,
                 user = DATABASE_USER,
                 password = DATABASE_PASSWORD
         )
 
         transaction (connect) {
-            SchemaUtils.create(Genres, Books, VoiceOvers, Libraries, Users, UserLibrary, BookLibrary)
+            SchemaUtils.create(Genres)
+            SchemaUtils.create(Books)
+            SchemaUtils.create(VoiceOvers)
+            SchemaUtils.create(Libraries)
+            SchemaUtils.create(Users)
+            SchemaUtils.create(UserLibrary)
+            SchemaUtils.create(BookLibrary)
         }
 
         return connect
+    }
+
+    private fun getDatabaseServerUrlConnection(): String {
+        return "jdbc:$DATABASE_TYPE://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?$CONNECTION_ENCODING&$USE_UNICODE"
     }
 
 }
